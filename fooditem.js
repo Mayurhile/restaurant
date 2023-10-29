ae55d132af99d7039aec8fd1df6400fc1c8ec034
Mayurhile <113536618+Mayurhile@users.noreply.github.com>
2023-10-29 22:23:55 +0530

    Add files via upload

---
+++ fooditem.js
@@ -0,0 +1,404 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Cappuccino',
+    category : 'coffee',
+    rating : 4.5,
+    price: 10,
+    img: 'images/Cappuccino-coffee.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Black Coffee',
+    category : 'coffee',
+    rating : 4.3,
+    price: 40,
+    img: 'images/Black-Coffee.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Latte',
+    category : 'coffee',
+    rating : 4.3,
+    price: 50,
+    img: 'images/Latte-coffee.jpg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Macchiato',
+    category : 'coffee',
+    rating : 4.3,
+    price: 50,
+    img: 'images/Macchiato-coffee.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Mocha',
+    category : 'coffee',
+    rating : 4.3,
+    price: 10,
+    img: 'images/Mocha-coffee.jpg',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Cold Coffee',
+    category : 'coffee',
+    rating : 4.3,
+    price: 12,
+    img: 'images/Cold-Coffee.jpg',
+    quantity: 1
+},
+{
+    id: 7,
+    name: 'Affogato',
+    category : 'coffee',
+    rating : 4.3,
+    price: 13,
+    img: 'images/Affogato-coffee.jpg',
+    quantity: 1
+},
+{
+    id: 8,
+    name: 'Iced Coffee',
+    category : 'coffee',
+    rating : 4.3,
+    price: 20,
+    img: 'images/Iced-Coffee.jpg',
+    quantity: 1
+},
+{
+    id: 9,
+    name: 'Americano',
+    category : 'coffee',
+    rating : 4.3,
+    price: 15,
+    img: 'images/Americano-coffee.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Chocolate Cake',
+    category : 'cakes',
+    rating : 4.3,
+    price: 11,
+    img: 'images/Chocolate-Cake.jpeg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Vanilla Cake',
+    category : 'cakes',
+    rating : 4.3,
+    price: 10,
+    img: 'images/Vanilla-Cake.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Red Velvet Cake',
+    category : 'cakes',
+    rating : 4.3,
+    price: 14,
+    img: 'images/Red-Velvet-Cake.jpeg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Carrot Cake',
+    category : 'cakes',
+    rating : 4.3,
+    price: 12,
+    img: 'images/Carrot-Cake.jpeg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Cheesecake',
+    category : 'cakes',
+    rating : 4.3,
+    price: 17,
+    img: 'images/Cheesecake.jpeg',
+    quantity: 1
+},
+{
+    id: 15,
+    name: 'Fruitcake',
+    category : 'cakes',
+    rating : 4.3,
+    price: 20,
+    img: 'images/Fruitcake.jpeg',
+    quantity: 1
+},
+{
+    id: 16,
+    name: 'Black Forest Cake',
+    category : 'cakes',
+    rating : 4.3,
+    price: 15,
+    img: 'images/Black-Forest-Cake.jpeg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Coffee Cake',
+    category : 'cakes',
+    rating : 4.3,
+    price: 10,
+    img: 'images/Coffee-Cake.jpeg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Tiramisu',
+    category : 'cakes',
+    rating : 4.3,
+    price: 15,
+    img: 'images/Tiramisu.jpeg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Pound Cake',
+    category : 'cakes',
+    rating : 4.3,
+    price: 12,
+    img: 'images/Pound-Cake.jpeg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Croissant',
+    category : 'pastries',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Croissant.jpeg',
+    quantity: 1
+},
+{
+    id: 21,
+    name: 'Eclair',
+    category : 'pastries',
+    rating : 4.3,
+    price: 7,
+    img: 'images/Eclair.jpeg',
+    quantity: 1
+},
+{
+    id: 22,
+    name: 'Danish Pastry',
+    age: '32',
+    category : 'pastries',
+    rating : 4.3,
+    price: 5,
+    img: 'images/Danish-Pastry.jpeg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Cannoli',
+    category : 'pastries',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Cannoli.jpeg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Strudel',
+    category : 'pastries',
+    rating : 4.3,
+    price: 7,
+    img: 'images/Strudel.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: 'Club Sandwich',
+    category : 'sandwich',
+    rating : 4.3,
+    price: 5,
+    img: 'images/Club-Sandwich.jpeg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Grilled Cheese',
+    category : 'sandwich',
+    rating : 4.3,
+    price: 7,
+    img: 'images/Grilled-Cheese.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Panini',
+    category : 'sandwich',
+    rating : 4.3,
+    price: 10,
+    img: 'images/Panini.jpeg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Turkey Chicken Club',
+    category : 'sandwich',
+    rating : 4.3,
+    price: 4,
+    img: 'images/Turkey-or-Chicken-Club.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Cuban Sandwich',
+    category : 'sandwich',
+    rating : 4.3,
+    price: 4,
+    img: 'images/Cuban-Sandwich.jpeg',
+    quantity: 1
+},
+{
+    id: 30,
+    name: 'Spaghetti',
+    category : 'pasta',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Spaghetti.jpeg',
+    quantity: 1
+},
+{
+    id: 31,
+    name: 'Fettuccine',
+    category : 'pasta',
+    rating : 4.3,
+    price: 7,
+    img: 'images/Fettuccine.jpeg',
+    quantity: 1
+},
+{
+    id: 32,
+    name: 'Penne',
+    category : 'pasta',
+    rating : 4.3,
+    price: 5,
+    img: 'images/Penne.jpeg',
+    quantity: 1
+},
+{
+    id: 33,
+    name: 'Farfalle',
+    category : 'pasta',
+    rating : 4.3,
+    price: 16,
+    img: 'images/Farfalle.jpeg',
+    quantity: 1
+},
+{
+    id: 34,
+    name: 'Rotini',
+    category : 'pasta',
+    rating : 4.3,
+    price: 14,
+    img: 'images/Rotini.jpeg',
+    quantity: 1
+},
+{
+    id: 35,
+    name: 'Rigatoni',
+    category : 'pasta',
+    rating : 4.3,
+    price: 10,
+    img: 'images/Rigatoni.jpg',
+    quantity: 1
+},
+{
+    id: 36,
+    name: 'Linguine',
+    category : 'pasta',
+    rating : 4.3,
+    price: 8,
+    img: 'images/Linguine.jpeg',
+    quantity: 1
+},
+{
+    id: 37,
+    name: 'Truffle Fries',
+    category : 'french fries',
+    rating : 4.3,
+    price: 18,
+    img: 'images/Truffle-Fries.jpg',
+    quantity: 1
+},
+{
+    id: 38,
+    name: 'Peri Peri Fries',
+    category : 'french fries',
+    rating : 4.3,
+    price: 20,
+    img: 'images/peri-peri-fries.jp g',
+    quantity: 1
+},
+{
+    id: 39,
+    name: 'Cajun Fries',
+    category : 'french fries',
+    rating : 4.3,
+    price: 12,
+    img: 'images/Cajun-Fries.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Curry Fries',
+    category : 'french fries',
+    rating : 4.3,
+    price: 10,
+    img: 'images/Curry-Fries.jpeg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Buffalo Fries',
+    category : 'french fries',
+    rating : 4.3,
+    price: 15,
+    img: 'images/Buffalo-Fries.jpeg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Pesto Fries',
+    category : 'french fries',
+    rating : 4.3,
+    price: 40,
+    img: 'images/Pesto-Fries.jpeg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Chili Cheese Fries',
+    category : 'french fries',
+    rating : 4.3,
+    price: 25,
+    img: 'images/Chili-Cheese-Fries.jpeg',
+    quantity: 1
+},
+{
+    id: 44,
+    name: 'Sweet Potato Fries',
+    category : 'french fries',
+    rating : 4.3,
+    price: 34,
+    img: 'images/Sweet-Potato-Fries.jpeg',
+    quantity: 1
+},
+]
+
+export {foodItem};
